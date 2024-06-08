c3ff5b3636433afa609d5fb85c80c66523c07494
Bohdan Oleksiuk <bohdan.oleksiuk@ecdevstudio.com>
2024-06-08 22:48:22 +0300

    findOne query

--- src/coffees/coffees.resolver.ts
+++ src/coffees/coffees.resolver.ts
@@ -1,10 +1,16 @@
-import { Resolver, Query } from '@nestjs/graphql';
+import { Resolver, Query, Args } from "@nestjs/graphql";
 import { Coffee } from "../graphql";
+import { ParseIntPipe } from "@nestjs/common";
 
 @Resolver()
 export class CoffeesResolver {
   @Query('coffees')
   async findAll(): Promise<Coffee[]> {
     return []
+  }
+
+  @Query('coffee')
+  async findOne(@Args('id', ParseIntPipe) id: number): Promise<Coffee> {
+    return null
   }
 }
